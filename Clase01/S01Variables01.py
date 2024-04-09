db449f914eee9a447cdf6f3dada97fad34894b4f
Juan Camilo <44326112+JuanCamilo18@users.noreply.github.com>
2024-04-08 23:57:07 -0500

    clase01-01

---
+++ Clase01/S01Variables01.py
@@ -0,0 +1,39 @@
+####### ----------------------------------- #######
+## Autor: Juan Camilo
+## Clase: Variables
+####### ----------------------------------- #######
+
+
+## Declaración de variables
+x = 10
+y = "Hola"
+
+## Impresión de variables
+
+print(x)
+print(y)
+
+## Tipos de datos
+entero = 10
+flotante = 3.14
+cadena = "Hola"
+lista = [1,2,3,4]
+tupla = (5,6,7,8)
+diccionario = {"nombre":"juan", "edad":40}
+
+## Imprimir tipo de datos
+print(type(entero))
+print(type(flotante))
+print(type(cadena))
+print(type(lista))
+print(type(tupla))
+print(type(diccionario))
+
+
+
+
+
+
+
+
+
---
+++ Clase01/S02Operadores.py
@@ -0,0 +1,26 @@
+
+####### ----------------------------------- #######
+## Autor: Juan Camilo
+## Clase: Operaciones
+####### ----------------------------------- #######
+
+### Operaciones Aritmeticos
+
+a = float(input("Ingresar el primer número: "))
+b = float(input("Ingresar el segundo número: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+
+### Operadores de asignacion
+x = 5
+x += 2
+
+
+
+
